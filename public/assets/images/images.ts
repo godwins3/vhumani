373ec5b03151cf3bee0db0b33294dbd4180716e0
cm-collins <dev-collins@outlook.com>
2025-05-28 04:31:53 +0000

    feat: add layout components and theme toggle functionality
    - Created Footer component for layout
    - Created Header component for layout
    - Implemented ModeToggle component for theme switching using next-themes
    - Added ThemeProvider component to manage theme context
    - Developed DropdownMenu component with various subcomponents for UI interactions

--- public/assets/images/images.ts
+++ public/assets/images/images.ts
@@ -1,1 +1,15 @@
-export {default as VhunamiLogo} from './vhunami-logo.png'
+export {default as VhumaniLogo} from './vhunami-logo.png';
+export {default as Belvia} from './Belivia.jpg';
+export {default as FindingMatches} from './Finding matches.png';
+export {default as Heroimg} from './Hero Image.jpg';
+export {default as HowVhumaniWorks} from './How Vhumani works 1.png';
+export {default as JacksonImg} from './Jackson.jpg';
+export {default as LauchingCampaign } from './Launching campaign.png';
+export {default as LisaImg} from './Lisa.jpg';
+export {default as  MeasuringResults} from './Measuring results.png';
+export {default as MikeImg} from './Mike.jpg';
+export {default as OurStory} from './Our Story.jpg';
+export {default as RitaImg} from './Rita.jpg';
+export {default as SarahImg} from './Sarah.jpg';
+export {default as TiktokImg} from './Tiktok.jpg'
+export {default as DashboardImg} from "./dashboard.jpg"
