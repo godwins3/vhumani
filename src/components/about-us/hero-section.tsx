758265d47c117d14cfbee8d1794281b6640229ab
godwins3 <praisegodwins4@gmail.com>
2025-06-11 09:44:06 +0300

    fix: update HeroSection heading color to enhance visibility

--- src/components/about-us/hero-section.tsx
+++ src/components/about-us/hero-section.tsx
@@ -2,7 +2,7 @@ const HeroSection = () => {
   return (
     <section className="bg-white-gradient-to-b from-white to-[#e8f2f5] text-center py-28 px-4 bg-white dark:bg-slate-900">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl md:text-6xl font-extrabold text-teal-50 dark:text-teal-100 leading-tight">
+        <h1 className="text-4xl md:text-6xl font-extrabold text-orange-500 dark:text-teal-100 leading-tight">
           Empowering Brands & Creators to Thrive Together
         </h1>
         <p className="text-gray-600 mt-6 text-lg md:text-xl">
